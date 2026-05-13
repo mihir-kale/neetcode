c69c9ce1a0f61d428e1af0c5a5b2b6e73f0babad
Mihir Kale <mihirpkale@gmail.com>
2026-05-13 14:07:45 -0400

    Add: car-fleet - submission-1

---
+++ Data Structures & Algorithms/car-fleet/submission-1.py
@@ -0,0 +1,17 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        
+    # if a car reaches the destination before or at the same time as a car ahead, 
+    # it must be part of the same fleet as that car,
+
+        pair = [(p, s) for p, s in zip(position, speed)]
+        pair.sort(reverse = True)
+        # pair is now sorted in desceding order by position
+
+
+        stack = [] # store the time remaining 
+        for p, s in pair: 
+            stack.append((target - p)/s)
+            if len(stack) >= 2 and stack[-1] <= stack[-2]: # is the closer car reaching before the next closest one
+                stack.pop()
+        return len(stack)
